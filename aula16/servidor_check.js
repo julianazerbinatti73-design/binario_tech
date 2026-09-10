c6b536707483faf6141a0f8395c2ed9e6ac80be0
julianazerbinatti73-desing <julianazerbinatti73@gmail.com>
2026-09-10 19:15:25 +0000

    eu alterei o arquivo servidor_check

--- aula16/servidor_check.js
+++ aula16/servidor_check.js
@@ -7,13 +7,12 @@ app.use(express.json());
 app.get('/api/v1/status-servidor', (req, res) => {
 	res.json({
 		status: "ONLINE",
-		ambiente: "Servidor Local de Prova - Binario Tech",
+		ambiente: "Servidor Local da Prova - Binario Tech",
 		usuario: process.env.USER || "aluno",
 		dataCheck: new Date()
 	});
 });
 
 app.listen(PORT, () => {
-	console.log(`[Binario Tech] Servidor de validacao da aula 16 ativo na porta ${PORT}`);
+	console.log(`[Binario Tech] Servidor de Validacao da aula 16 ativo na porta ${PORT}`);
 });
-
